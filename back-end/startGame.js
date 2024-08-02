98d6621b730e9cb2e07aa6233f360508e35cb304
Tyler Herritt <ty373633@dal.ca>
2024-08-02 18:28:59 -0300

    Added websocket and basic lambda routes to cloudformation

--- back-end/startGame.js
+++ back-end/startGame.js
@@ -1,18 +1,12 @@
-import {
-    ApiGatewayManagementApiClient,
-    PostToConnectionCommand,
-  } from "@aws-sdk/client-apigatewaymanagementapi";
-
-
-  const client = new ApiGatewayManagementApiClient({ endpoint: CONNECTION_URL });
+import { ApiGatewayManagementApiClient, PostToConnectionCommand} from "@aws-sdk/client-apigatewaymanagementapi";
+const client = new ApiGatewayManagementApiClient({ endpoint: CONNECTION_URL });
   
-  export const handler = async (event) => {
+export const handler = async (event) => {
     const connectionId = event.requestContext.connectionId;
     const message = new PostToConnectionCommand({ ConnectionId: connectionId, Data: "Game started" });
     await client.send(message);
-    const response = {
-      statusCode: 200
-    };
+
+    const response = { statusCode: 200 };
     return response;
-  };
+};
   
